a4b8997be6d401724d807ffba67657a311ec0cb5
Ian-c-cameron <ian.c.cam@gmail.com>
2020-11-10 04:50:32 +0000

    exercise 7 complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+Employee.validates :first_name, presence: true
+Employee.validates :last_name, presence: true
+Employee.validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
+Employee.validates :store_id, numericality: { only_integer: true, greater_than: 0 }
+
+Store.validates :name, length: { minimum: 3 }
+Store.validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+# Store.validates :must_have_aparel see ../lib/store.rb 
+
+puts "Enter a store name:"
+new_store = gets.chomp
+@invalid_store = Store.create(name: new_store)
+pp @invalid_store.errors.messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,9 @@
 class Store < ActiveRecord::Base
+  validate :must_have_aparel, on: :create
 
+  def must_have_aparel
+    if !womens_apparel && !mens_apparel
+      errors.add(:womens_apparel, "can't be false if :mens_apperel is false")
+    end
+  end
 end
